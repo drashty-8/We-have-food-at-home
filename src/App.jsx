5cadf780cb60ed85e2ec86ceeeafdb42d41bbbe3
iitza <168257540+iitza@users.noreply.github.com>
2026-04-16 14:14:02 -0700

    Add Login and Register routes to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 import Account from "./pages/Account";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/account" element={<Account />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </main>
     </>

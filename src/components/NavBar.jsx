e100af687e9e3d4431b8f61f967bc6d63f6246bf
iitza <168257540+iitza@users.noreply.github.com>
2026-04-20 19:07:29 -0700

    Implement user authentication links in NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,29 @@
 import "../css/NavBar.css";
 import { Link, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../config/firebase"; 
 
 function NavBar() {
   const location = useLocation();
   const hideOn = ["/login", "/register"];
+  const [user, setUser] = useState(null); 
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    })
+    return () => unsubscribe(); 
+  }, []);
 
   if (hideOn.includes(location.pathname)) return null;
+
+  let authLink;
+  if (user) {
+    authLink = <Link to= "/account">Account</Link>;
+  } else {
+    authLink = <Link to= "/login">Login</Link>;
+  }
   
   return (
     <nav className="navbar">
@@ -14,7 +32,7 @@ function NavBar() {
       </div>
       <div className="navbar-links">
         <Link to="/">Home</Link>
-        <Link to="/login">Login</Link>
+        {authLink}
       </div>
     </nav>
   );

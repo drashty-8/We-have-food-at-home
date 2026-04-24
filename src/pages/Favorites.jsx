2ccd4e77a0afea5749c1b1109d6920494d386482
drashty-8 <157762977+drashty-8@users.noreply.github.com>
2026-04-23 21:13:12 -0700

    Add files via upload

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Favorites() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Your Favorites</h1>
+      <p>This is where your saved recipes will appear!</p>
+    </div>
+  );
+}
+
+export default Favorites;

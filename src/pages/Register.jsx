ff7acb97044f05e3ffde732b9cb6a69445362c48
Devonte Renshaw <devonte@devonterenshaw.com>
2026-04-27 18:59:52 -0700

    Fix bug due to typo

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,7 +8,7 @@ export default function Register(){
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
-    const navigate = useNavigate;
+    const navigate = useNavigate();
 
     const handleRegister = async(e) => {
         e.preventDefault();
